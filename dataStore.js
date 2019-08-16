35e731482bb08888876364d88a0ddb7967948f5c
Joey Romo <joeyjr95@gmail.com>
2019-08-16 15:21:46 -0400

    added questions to dataStore

---
+++ dataStore.js
@@ -0,0 +1,54 @@
+'use strict';
+const data = [
+  {
+    question: 'Which of the following races is not available to players?',
+    answers: ['Pixie','Simic Hybrid','Goblin','Tortle'],
+    correctAnswer: 0,
+  },
+  {
+    question: 'Which of the following classes is not available to players?',
+    answers: ['Druid','Wizard','Sorcerer','Thief'],
+    correctAnswer: 3,
+  },
+  {
+    question: 'Which of the following races has the highest Intelligence bonus?',
+    answers: ['Dragonborn','Gnome','Centaur','Elf'],
+    correctAnswer: 1,
+  },
+  {
+    question: 'Which of the following classes has the most Hit Die?',
+    answers: ['Fighter','Monk','Cleric','Barbarian'],
+    correctAnswer: 3,
+  },
+  {
+    question: 'Which of the following races has the most number of subraces?',
+    answers: ['Lizardfolk','Elf','Tiefling','Human'],
+    correctAnswer: 2,
+  },
+  {
+    question: 'Which of the following classes does not have Spellcasting as a base class feature?',
+    answers: ['Warlock','Paladin','Bard','Ranger'],
+    correctAnswer: 0,
+  },
+  {
+    question: 'Which of the following is an ability that measures a character’s reflexes and balance?',
+    answers: ['Agility','Athletics','Dexterity','Acrobatics'],
+    correctAnswer: 2,
+  },
+  {
+    question: 'Which of the following is not a damage type?',
+    answers: ['Bludgeoning','Chopping','Force','Thunder'],
+    correctAnswer: 1,
+  },
+  {
+    question: 'What is the highest possible ability score that a character can have?',
+    answers: ['Unlimited','20','18','30'],
+    correctAnswer: 3,
+  },
+  {
+    question: 'Which of the following is not a condition?',
+    answers: ['Invisible','Exhausted','Prone','Charmed'],
+    correctAnswer: 1,
+  },
+    
+];

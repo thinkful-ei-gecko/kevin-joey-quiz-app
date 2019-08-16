bc1878286c537a0d229c6fb9e5fafe4a3ab1e892
Joey Romo <joeyjr95@gmail.com>
2019-08-16 17:05:36 -0400

    stuff

--- index.js
+++ index.js
@@ -18,39 +18,45 @@ function generateQuestion(){
         <form>
           <fieldset>
             <label class="answerOption">
-              <input type="radio" value="Answer" name="answer" required="">
+              <input type="radio" value="${data[questionNumber].answers[0]}" name="answer" required="">
               <span>${data[questionNumber].answers[0]}</span>
             </label>
             <label class="answerOption">
-              <input type="radio" value="Answer" name="answer" required="">
+              <input type="radio" value="${data[questionNumber].answers[1]}" name="answer" required="">
               <span>${data[questionNumber].answers[1]}</span>
             </label>
             <label class="answerOption">
-              <input type="radio" value="Answer" name="answer" required="">
+              <input type="radio" value="${data[questionNumber].answers[2]}" name="answer" required="">
               <span>${data[questionNumber].answers[2]}</span>
             </label>
             <label class="answerOption">
-              <input type="radio" value="Answer" name="answer" required="">
+              <input type="radio" value="${data[questionNumber].answers[3]}" name="answer" required="">
               <span>${data[questionNumber].answers[3]}</span>
             </label>
-            <button type="submit" class="submitButton">Submit</button>
+            <p class="correct-or-wrong-text"></p>
+            <button type="button" class="submitButton">Submit</button>
           </fieldset>
         </form>
       </div>
     </section>
   `);
 }
-function checkAnswer(){
-  $('form').on('click keypress', event =>{
+
+function handleSubmit(){
+  $('main').on('click keypress', '.submitButton', event => {
     let selectedAnswer = $('input:checked').val();
-    let correctAnswer = `${data[questionNumber].answers[correctAnswer]}`;
-    if( selectedAnswer === correctAnswer){
-        
+    console.log(selectedAnswer);
+    let rightAnswer = `${data[questionNumber].answers[data[questionNumber].correctAnswer]}`;
+    console.log(rightAnswer);
+    if( selectedAnswer === rightAnswer){
+      $('.correct-or-wrong-text').text('CORRECT');
     }
-    else{}
+    else{ 
+      $('.correct-or-wrong-text').text('WRONG');
+    }
   });
 }
 $(()=>{
   startQuiz();
-  
+  handleSubmit();
 });

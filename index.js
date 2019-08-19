99277a266955e276ce346aaf7fbf4b53c583a068
Joey Romo <joeyjr95@gmail.com>
2019-08-18 20:31:30 -0400

    styled for mobile almost complete

--- index.js
+++ index.js
@@ -3,16 +3,20 @@
 let questionNumber = 0;
 let amountCorrect = 0;
 let amountWrong = 0;
+const diceImage = `<img src="https://png2.kisspng.com/sh/dc93024927e664ccce5e3d60c5d4694a/L0KzQYm3VsA4N6lAfZH0aYP2gLBuTfQzOF54keV9ZX2wdLrqhb1lfZ9sfdH3cz3ngrLujB50NZ56jNN3dIOwfbL6lPUubGM1RadrMUnmRbS7UvVmapI8Rqk5N0G5R4m9UcUzQGU2TagDNEG8QoO1kP5o/kisspng-d20-system-dice-dungeons-dragons-mutants-maste-d20-5b19c5c42eeba7.7071678615284156841922.png" alt= "a 20 sided dice for dungeons and dragons">`;
 
 function startQuiz(){
   $('main').on('click keypress', '.startButton', () => {
     $('.startScreen').remove(); 
     generateQuestion();
     updateScore();
+    $('header').append(`${diceImage}`);
   });
+  
 }
 
 function generateQuestion(){
+  $('body').css('background-color', 'white');
   $('main').append(`
     <section class="quizForm">
       <div class="question-1">
@@ -92,6 +96,7 @@ function handleNext(){
 }
 
 function updateScore(){
+  
   $('.score').text(`${amountCorrect} correct, ${amountWrong} wrong`);
 }
 
@@ -129,7 +134,7 @@ function initializeStartScreen(){
   $('main').append(`
     <section class="startScreen">
       <h1>How Well Do You Know "Dungeons & Dragons"?</h1>
-      <img src="https://png2.kisspng.com/sh/dc93024927e664ccce5e3d60c5d4694a/L0KzQYm3VsA4N6lAfZH0aYP2gLBuTfQzOF54keV9ZX2wdLrqhb1lfZ9sfdH3cz3ngrLujB50NZ56jNN3dIOwfbL6lPUubGM1RadrMUnmRbS7UvVmapI8Rqk5N0G5R4m9UcUzQGU2TagDNEG8QoO1kP5o/kisspng-d20-system-dice-dungeons-dragons-mutants-maste-d20-5b19c5c42eeba7.7071678615284156841922.png" alt= "a 20 sided dice for dungeons and dragons">
+      ${diceImage} 
       <button type="button" class="startButton">Begin Quiz</button>
     </section>
   `);
